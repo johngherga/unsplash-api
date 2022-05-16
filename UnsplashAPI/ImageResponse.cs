67aab031b4fcaefc971bbe738176c44bb268ddc2
john_gherga <john.gherga@softhouse.se>
2022-05-16 15:08:30 +0200

    First commit

---
+++ UnsplashAPI/ImageResponse.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace UnsplashAPI
+{
+    public class ImageResponse
+    {
+        public ImageResponse()
+        {
+        }
+
+        [JsonPropertyName("id")]
+        public string Id { get; set; } = string.Empty;
+        [JsonPropertyName("created_at")]
+        public string CreatedAt { get; set; }
+        [JsonPropertyName("updated_at")]
+        public string UpdatedAt { get; set; }
+        [JsonPropertyName("width")]
+        public int Width { get; set; }
+        [JsonPropertyName("height")]
+        public int Height { get; set; }
+        [JsonPropertyName("color")]
+        public string Color { get; set; }
+        [JsonPropertyName("blur_hash")]
+        public string BlurHash { get; set; }
+        [JsonPropertyName("downloads")]
+        public int Downloads { get; set; }
+        [JsonPropertyName("likes")]
+        public int Likes { get; set; }
+        [JsonPropertyName("liked_by_user")]
+        public bool LikedByUser { get; set; }
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+    }
+
+    class Exif
+    {
+        [JsonPropertyName("make")]
+        public string Make { get; set; }
+        [JsonPropertyName("model")]
+        public string Model { get; set; }
+        [JsonPropertyName("exposure_time")]
+        public string ExposureTime { get; set; }
+        [JsonPropertyName("aperture")]
+        public string Aperture { get; set; }
+        [JsonPropertyName("focal_length")]
+        public string FocalLength { get; set; }
+        [JsonPropertyName("iso")]
+        public int Iso { get; set; }
+    }
+}
---
+++ UnsplashAPI/UnsplashAPI.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace UnsplashAPI
+{
+    public static class UnsplashAPI
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        [FunctionName("GetImageDaily")]
+        public static void GetImageDaily([TimerTrigger("0012**?")] TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+
+        [FunctionName("GetRandomImage")]
+        public static async Task<IActionResult> GetRandomImage(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "image")] HttpRequest request,
+            ILogger logger)
+        {
+            logger.LogInformation($"Getting image from unsplash API.");
+
+            client.DefaultRequestHeaders.Add("Accept-Version", "v1");
+            client.DefaultRequestHeaders.Authorization =
+    new AuthenticationHeaderValue("Client-ID", "6P1j7UZBWSnsLt5Fwg6kq6m_UCRbqee_rc4PLcjxHZg");
+
+            HttpResponseMessage response = await client.GetAsync("https://api.unsplash.com/photos/random");
+
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            logger.LogInformation($"Image id: {responseBody}");
+
+            return new OkObjectResult(responseBody);
+        }
+
+        [FunctionName("GetImageStat")]
+        public static async Task<IActionResult> GetImageStat(
+    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "image/stat")] HttpRequest request,
+    ILogger logger)
+        {
+            logger.LogInformation($"Getting image statistics from unsplash API.");
+
+            client.DefaultRequestHeaders.Add("Accept-Version", "v1");
+            client.DefaultRequestHeaders.Authorization =
+    new AuthenticationHeaderValue("Client-ID", "6P1j7UZBWSnsLt5Fwg6kq6m_UCRbqee_rc4PLcjxHZg");
+
+            HttpResponseMessage response = await client.GetAsync("https://api.unsplash.com/photos/:id/statistics");
+
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+
+            return new OkObjectResult(responseBody);
+        }
+    }
+}
